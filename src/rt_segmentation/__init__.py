e09322bfcf504cc18a53cc956cf3f90d22a324f6
LeonHammerla <leon.hammerla@gmx.de>
2026-01-28 11:48:09 +0100

    added surprisal based splitting

--- src/rt_segmentation/__init__.py
+++ src/rt_segmentation/__init__.py
@@ -4,3 +4,4 @@ from .seg_utils import bp, sdb_login, load_prompt, load_example_trace
 from .llm_split_offset import RTLLMOffsetBased
 from .llm_split_sent_chunks import RTLLMSentBased
 from .llm_split_forced_decoder import RTLLMForcedDecoderBased
+from .llm_split_surprisal import RTLLMSurprisal
